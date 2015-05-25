07fe39779d691e0764380c5b14147ccd5c7d057c
Michael Crivello <michaelacrivello@gmail.com>
2015-05-24 23:25:09 -0700

    changing Game model to have Round and UserDigest lists instead of ObjectIds

--- app/src/main/java/com/michaelcrivello/apps/snaphunt/data/model/Game.java
+++ app/src/main/java/com/michaelcrivello/apps/snaphunt/data/model/Game.java
@@ -6,9 +6,8 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Created by michael on 3/22/15.
+ * Game model.
  */
-// TODO: ObjectID and Date de/serializer
 // http://craigsmusings.com/2011/04/09/deserializing-mongodb-ids-and-dates-with-gson/
 
 public class Game {
@@ -17,8 +16,8 @@ public class Game {
     int numberOfRounds;
     int currentRound;
     //TODO: currently Object Ids in model schema
-    List<ObjectId> rounds;
-    List<ObjectId> players; // players will be UserDigests
+    List<Round> rounds;
+    List<UserDigest> players; // players will be UserDigests
     Date timeCreated;
     Date timeLastModified;
     Date timeEnded;
@@ -41,11 +40,11 @@ public class Game {
         return currentRound;
     }
 
-    public List<ObjectId> getRounds() {
+    public List<Round> getRounds() {
         return rounds;
     }
 
-    public List<ObjectId> getPlayers() {
+    public List<UserDigest> getPlayers() {
         return players;
     }
 
